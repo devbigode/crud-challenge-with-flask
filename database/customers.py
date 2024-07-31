581e99110dfd92cc03ac1ff1b95666929419931f
Guilherme Morais <guilherme.ads@proton.me>
2024-07-30 22:02:18 -0300

    Update customers.py

--- database/customers.py
+++ database/customers.py
@@ -5,9 +5,5 @@ import os
 load_dotenv()
 
 db_name = os.getenv('POSTGRES_URL')
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
-db_host = os.getenv('DB_HOST')
-db_port = int(os.getenv('DB_PORT'))
 
 db = PostgresqlDatabase(db_name)
