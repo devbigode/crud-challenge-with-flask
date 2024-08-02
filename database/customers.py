5d2c02728c96eb43c638de8d5f25553b743f8570
Guilherme Morais <Guilherme>
2024-08-01 21:56:29 -0300

    Uso de recursos do jinja2 (extends)

--- database/customers.py
+++ database/customers.py
@@ -1,13 +1,3 @@
-from peewee import PostgresqlDatabase
-from dotenv import load_dotenv
-import os
+from peewee import SqliteDatabase
 
-load_dotenv()
-
-db_name = os.getenv('POSTGRES_URL')
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
-db_host = os.getenv('DB_HOST')
-db_port = int(os.getenv('DB_PORT'))
-
-db = PostgresqlDatabase(db_name)
+db = SqliteDatabase('local_db')
